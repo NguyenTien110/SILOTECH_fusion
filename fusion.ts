49975184be7982d86ca2a027e804ce1b6ecd4de9
Nguyễn Văn Tiến <50447619+NguyenTien110@users.noreply.github.com>
2022-01-07 10:25:25 +0700

    Update fusion.ts

--- fusion.ts
+++ fusion.ts
@@ -6,19 +6,19 @@ const formulaString = ["Common", "Rare", "Epic", "Legendary"]
 const luckyPotion = [7, 7, 7, 7, 7, 5, 5, 3, 3, 1, 1]
 const successRateInit = [80, 60, 40, 30, 25, 15, 15, 5, 3, 1, 0.5]
 
-const getFusionResult = (_user: string, _buy_blockhash: string, _runeId: number, _lucky_potion: boolean): string => {
+const getFusionResult = (_user: string, _fusion_blockhash: string, _runeId: number, _lucky_potion: boolean): string => {
     if (_runeId < 0 || _runeId > 10) throw new Error(`_runeId must be in range 0 and 10`)
 
     var lucky = _lucky_potion === true ? luckyPotion[_runeId] : 0
 
-    var seed = seedGenerator(_user, _buy_blockhash, _runeId, amountOfFusion[_runeId], formula[_runeId], lucky)
+    var seed = seedGenerator(_user, _fusion_blockhash, _runeId, amountOfFusion[_runeId], formula[_runeId], lucky)
 
     var prob = parseInt('0x' + seed, 16) % 1000
     
     if (prob < (successRateInit[_runeId] + lucky) * 10) 
-        return "success"
+        return true
     else 
-        return "fail"
+        return false
 }
 
 //const rune = 0;
